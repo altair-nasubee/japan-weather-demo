b5813dd07d2f1fb9888b8c2df9769a418296726c
Kiyoshi Ishiyama <altair@nasubee.com>
2026-06-28 23:52:12 +0900

    chore: add runtime/editor/test assembly definitions

---
+++ UnityProject/Assets/Tests/EditMode/SmokeTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace JapanWeatherDemo.Tests
+{
+    public class SmokeTest
+    {
+        [Test]
+        public void TestAssembly_Compiles_And_Runs()
+        {
+            Assert.AreEqual(4, 2 + 2);
+        }
+    }
+}
